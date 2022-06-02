e6282b033c4e64659f49eb151813713845b375c0
jkyoyo254 <janetkyoyo@gmail.com>
2022-06-02 08:10:52 -0700

    my puts commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-
-/**
- * main-prints the phrase "programming is like building a multilingual puzzle
- * Return : 0 if exited properly, non-zero otherwise
- */
-int main (void)
- {
-   puts("\" programming is like building a multilingual puzzle");
-   return (0);
- }
+  
+     /**
+      * Main-prints the phrase "programming is like building a multilingual puzzle"
+      * Return : 0 if exited properly, non-zero otherwise
+      */
+    int main(void)
+    {
+       Puts ("\" programming is like building a multilingual puzzle");
+       return (0);
+    }
