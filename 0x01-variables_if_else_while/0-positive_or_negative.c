a6eafc43ee862c29cf3ecac6283fe064542702ed
jkyoyo254 <janetkyoyo@gmail.com>
2022-06-03 05:19:37 -0700

    my +ve _ve commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,12 +2,11 @@
 #include <time.h>
 #include <stdio.h>
 /**
- *main - Entry point
+ * main - Entry point
  *
  * Return: Always 0 (success)
  */
 int main(void)
-
 {
 int n;
 
@@ -17,7 +16,7 @@ n = rand() - RAND_MAX / 2;
 
 if (n==0)
 {
-printf('%i is zero\n', n)
+printf('%i is zero\n', n);
 }
 
 else if (n<0)
