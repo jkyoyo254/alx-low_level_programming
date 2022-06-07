751fa46a81436d5cb4653a37b21f04091886f2d1
jkyoyo254 <janetkyoyo@gmail.com>
2022-06-07 00:22:06 -0700

    my commit 3

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * print the alphabet in lowercase
+ *
+ * return: Always 0 (Success)
+ */
+int main(void)
+{
+int ch;
+for (ch = 'a'; ch <= '2'; ch++)
+{
+putchart(ch);
+}
+
+putchart('\n');
+
+return (0);
+}
