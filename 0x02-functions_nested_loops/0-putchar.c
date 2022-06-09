939d46d34a15c699d3b7179f04b2d9d40f3c0dad
jkyoyo254 <janetkyoyo@gmail.com>
2022-06-09 03:57:06 -0700

    my task 0 commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,17 @@
 #include "main.h"
-
 /**
  * main - Entry
  *
  * Return: 0
  */
-
 int main(void)
 {
-	 char word[9] = "_putchar";
-	 
-	 int i = 0;
+	char word[9] = "_putchar";
 
-	 
-	while (i < 8
+	int i = 0;
+
+
+	while (i < 8)
 	{
 		_putchar(word[i]);
 		i++;
