88e40dac7764dcd14d93b5dccd406e82e840fb8f
jkyoyo254 <janetkyoyo@gmail.com>
2022-06-12 03:05:05 -0700

    more numbers

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,15 +6,14 @@
  */
 void print_most_numbers(void)
 {
-	int i;
-	i = _isalpha;
+	int i = 0; 
 
 	for (i = 0; i <= 9; i++)
 	{
 		if ((i == 2) || (i == 4))
 			continue;
 		else
-			_putchar(1 + '0');
+			_putchar(i + '0');
 	}
 	_putchar('\n');
 }
