7b2146eac06f9a2aa45eeb0a1875178fabb477b5
jkyoyo254 <janetkyoyo@gmail.com>
2022-06-14 07:05:20 -0700

    my swap commit

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - swaps the values of 2 integers
+ * @a: parameter 1
+ * @b: parameter 2
+ */
+void swap_int(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
