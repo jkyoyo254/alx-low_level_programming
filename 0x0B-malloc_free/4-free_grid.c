7b9d0b8b0363b2608055f7a91b856b64122cf38c
jkyoyo254 <janetkyoyo@gmail.com>
2022-06-28 06:38:28 -0700

    malloc

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - frees a2 dimensinal grid
+ * @grid: multidimensional arry of integers
+ * @height: height of the grid 
+ *
+ * Return: no return
+ */
+void free_grid(int **grid, int height)
+{
+	if (grid != NULL && height != 0)
+	{
+		for (; height >= 0; height--)
+			free(grid[height]);
+		free(grid);
+	}
+}
