b75c74ec9865791f60118af5dd75f047069d5513
jkyoyo254 <janetkyoyo@gmail.com>
2022-06-28 14:01:42 -0700

    hot fix

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,7 +16,7 @@ int **alloc_grid(int width, int height)
 	if (width < 1 || height < 1)
 		return (NULL);
 
-	gridout = malloc(height * size of(int *));
+	gridout = malloc(height * sizeof(int *));
 	if (gridout == NULL)
 	{
 		free(gridout);
@@ -34,4 +34,10 @@ int **alloc_grid(int width, int height)
 			return (NULL);
 		}
 	}
+
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
+			gridout[i][j] = 0;
+
+	return (gridout);
 }
