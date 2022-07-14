a5dc01d3c920c188072aa8b0604e485602984727
jkyoyo254 <janetkyoyo@gmail.com>
2022-07-14 12:43:56 -0700

    free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "list.h"
+
+/**
+ * free_list - free a list
+ * @head: head of the linked list
+ * Return: no return
+ */
+
+void free_list(list_t *head)
+{
+	list_t *current;
+
+	while((current = head) != NULL)
+	{
+		head = head->next;
+		free(current->str);
+		free(current);
+	}
+}
