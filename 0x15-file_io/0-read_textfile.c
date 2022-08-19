482133f4fc2b46e0f702bafd5d2d6d9f3c05d528
jkyoyo254 <janetkyoyo@gmail.com>
2022-08-19 08:19:51 -0700

    0-read-text

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,12 +5,12 @@
  * @filename: filename
  * @letters: numbers of letters printed
  *
- * Return: numbers of letters printed. I fails, returns 0
+ * Return: numbers of letters printed. if fails returns 0
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t nrd, nwr;
+	ssize_t nrd, nrw;
 	char *buf;
 
 	if (!filename)
@@ -26,8 +26,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	nrd = read(fd, buf, letters);
-	nwr = write(STDOUT_FILENO, buf , nrd);
-	
+	nwr = write(STDOUT_FILENO, buf, nrd);
+
 	close(fd);
 
 	free(buf);
