727465538bd637500fedfe24a951fdb7594f15f6
jkyoyo254 <janetkyoyo@gmail.com>
2022-08-22 04:09:58 -0700

    create file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,7 +7,7 @@
  *
  * Return: 1 if succeful.-1 if not
  */
-int creates_file(const char *filename, char *text_content)
+int create_file(const char *filename, char *text_content)
 {
 	int fd;
 	int nletters;
